6c61cd729ca93bd982f06e2082a660b0da0de07e
Jacob Brunson <jacob.brunson@vivint.com>
2023-04-28 16:49:34 -0600

    Resolve issue #6

--- examples/example_cpp.py
+++ examples/example_cpp.py
@@ -1,20 +1,23 @@
 import asyncapicodegen.cpp
-import sys
+import os
 import yaml
-import stringcase
-from pprint import pprint
-import urllib.request
+
 
 if __name__ == '__main__':
 
-    with urllib.request.urlopen("https://raw.githubusercontent.com/asyncapi/asyncapi/master/examples/2.0.0/streetlights.yml") as fp:
+    this_directory = os.path.dirname(os.path.abspath(__file__))
+    output_directory = os.path.join(this_directory, "output_cpp")
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    with open(os.path.join(this_directory, "streetlights-mqtt.yml")) as fp:
         spec = yaml.load(fp, Loader=yaml.FullLoader)
 
     # Dumb yaml importer interprets "on"/"off" as True/False
     spec['components']['schemas']['turnOnOffPayload']['properties']['command']['enum'] = ["on", "off"]
 
-    name = 'streetlights'
+    name = 'Streetlights'
 
     resolver = asyncapicodegen.cpp.SimpleResolver(name)
-    generator = asyncapicodegen.cpp.GeneratorFromAsyncApi('output', 'output', resolver, ['ex'], [['ex']])
+    generator = asyncapicodegen.cpp.GeneratorFromAsyncApi(output_directory, output_directory, resolver, ['ex'], [['ex']])
     generator.Generate(spec, name)
